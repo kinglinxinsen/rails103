071277eada4c6567ec37e1f72dd746163d4c9964
zengjinlin <kinglinxinsen@gmail.com>
2017-06-17 14:48:18 +0800

    implement group/edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,8 +13,14 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
-  end 
+  end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+  end
   private
 
   def group_params
